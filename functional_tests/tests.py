a39e1cd37ccd66cd2c998b2966856d4d839ac481
Jan Dąbrowski <jan.dabrowski@ilabo.com.pl>
2020-08-31 01:18:18 +0200

    move functional tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,9 +3,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
+from django.test import LiveServerTestCase
 import unittest
 
-class CvVisitorTest(unittest.TestCase):  
+class CvVisitorTest(LiveServerTestCase):  
 
     def setUp(self):  
         self.browser = webdriver.Firefox()
@@ -15,7 +16,7 @@ class CvVisitorTest(unittest.TestCase):
 
     def test_can_enter_cv(self):
         # Jan can see a home page
-        self.browser.get('http://127.0.0.1:8000')
+        self.browser.get(self.live_server_url)
         self.assertIn('Jan\'s blog', self.browser.title)
 
         navbar = self.browser.find_element_by_id("navbarResponsive")
@@ -35,7 +36,7 @@ class CvVisitorTest(unittest.TestCase):
 
     def test_can_see_cv(self):
         # Jan accesses a CV subpage
-        self.browser.get('http://127.0.0.1:8000/cv')
+        self.browser.get(self.live_server_url + '/cv')
 
         # He notices the page header mention cv
         header_text = self.browser.find_element_by_tag_name('h1').text  
@@ -51,12 +52,12 @@ class CvVisitorTest(unittest.TestCase):
             self.assertIn(title, section_title)
 
     def test_can_add_cv_section(self):
-        self.browser.get('http://127.0.0.1:8000/cv/new')
+        self.browser.get(self.live_server_url + '/cv/new')
         form = self.browser.find_element_by_tag_name('form')
         
         # Select CV section
         select_section = Select(form.find_element_by_name('section'))
-        select_section.select_by_visible_text('Professional experience')
+        select_section.select_by_visible_text('Professional Experience')
         
         # Provide title
         title_element = form.find_element_by_name("title")
@@ -81,8 +82,5 @@ class CvVisitorTest(unittest.TestCase):
         self.assertIn('Jan Dabrowski', header_text)
 
         section = self.browser.find_element_by_xpath("//div[@id='professionalExperience']")
-        section_items = section.find_element_by_class_name('item')
+        section_items = section.find_elements_by_class_name('item')
         self.assertEqual(1, len(section_items))
-
-if __name__ == '__main__':  
-    unittest.main() 
