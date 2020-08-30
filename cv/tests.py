1a09431d5615828342907933f77f235a47f71cc5
Jan Dąbrowski <jan.dabrowski@ilabo.com.pl>
2020-08-30 22:28:23 +0200

    Test blog

--- cv/tests.py
+++ cv/tests.py
@@ -1,13 +1,18 @@
 from django.urls import resolve
 from django.test import TestCase
-# from lists.views import home_page  
+from blog.views import post_list
+from django.http import HttpRequest
 
-# class HomePageTest(TestCase):
-#     def test_root_url_resolves_to_home_page_view(self):
-#         found = resolve('/')  
-#         self.assertEqual(found.func, home_page)
+class HomePageTest(TestCase):
 
-class SmokeTest(TestCase):
+    def test_root_url_resolves_to_post_list_view(self):
+        found = resolve('/')  
+        self.assertEqual(found.func, post_list)
 
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()  
+        response = post_list(request)  
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('\n<!doctype html>'))  
+        self.assertIn('<title>Jan\'s blog</title>', html)  
+        self.assertTrue(html.endswith('</html>'))
